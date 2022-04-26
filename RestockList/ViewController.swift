e127b1c12056b0fc6d03f591ce9aee31d22287c0
github user name <yazujumusa@gmail.com>
2022-04-26 20:09:05 +0900

    Move TableViewCell into ViewController

--- RestockList/ViewController.swift
+++ RestockList/ViewController.swift
@@ -14,11 +14,30 @@ class ViewController: UITableViewController, EditProtocol, UpdateProtocol {
     let realm = try! Realm()
     var currentDate: Int?
     var lastDate: Int?
+    var editUIViewController = EditUIViewController()
+    var myViewController = ViewController()
+    var delegate : EditProtocol?
+    var updateDelegate: UpdateProtocol?
     @IBOutlet var myTableView: UITableView!
+    @IBOutlet weak var periodLabelLeft: UILabel!
+    @IBOutlet weak var periodLabelRight: UILabel!
+    @IBOutlet weak var itemLabel: UILabel!
+    @IBOutlet weak var itemFrame: UIView!
+    @IBOutlet weak var periodFrame: UIView!
+    @IBOutlet weak var editButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var checkButton: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
+        itemFrame.layer.cornerRadius = 15
+        periodFrame.layer.borderWidth = 3
+        periodFrame.layer.borderColor = UIColor.tintColor.cgColor
+        progressBar.transform = CGAffineTransform(scaleX: 1, y: 11)
+        data = realm.objects(TableViewItem.self).sorted(by: { $0.remainingTime < $1.remainingTime })
         
         currentDate = Int(Date().timeIntervalSince1970)
         if let current = currentDate, let last = lastDate {
@@ -73,5 +92,38 @@ class ViewController: UITableViewController, EditProtocol, UpdateProtocol {
     
     func updateTableView() {
         tableView.reloadData()
+    }
+    
+    @IBAction func editButtonTapped(_ sender: UIButton) {
+        UINotificationFeedbackGenerator().notificationOccurred(.success)
+        delegate?.catchData(selectedCell: sender.tag)
+    }
+    
+    @IBAction func plusButtonTapped(_ sender: UIButton) {
+        if realm.object(ofType: TableViewItem.self, forPrimaryKey: sender.tag)!.remainingTime < realm.object(ofType: TableViewItem.self, forPrimaryKey: sender.tag)!.period {
+            realm.beginWrite()
+            realm.object(ofType: TableViewItem.self, forPrimaryKey: sender.tag)!.remainingTime += 1
+            try! realm.commitWrite()
+            updateDelegate?.updateTableView()
+            UIImpactFeedbackGenerator(style: .heavy).impactOccurred()
+        }
+    }
+    
+    @IBAction func minusButtonTapped(_ sender: UIButton) {
+        if realm.object(ofType: TableViewItem.self, forPrimaryKey: sender.tag)!.remainingTime > 0 {
+            realm.beginWrite()
+            realm.object(ofType: TableViewItem.self, forPrimaryKey: sender.tag)!.remainingTime -= 1
+            try! realm.commitWrite()
+            updateDelegate?.updateTableView()
+            UIImpactFeedbackGenerator(style: .heavy).impactOccurred()
+        }
+    }
+    
+    @IBAction func checkButtonTapped(_ sender: UIButton) {
+        UINotificationFeedbackGenerator().notificationOccurred(.success)
+        realm.beginWrite()
+        realm.object(ofType: TableViewItem.self, forPrimaryKey: sender.tag)!.remainingTime = realm.object(ofType: TableViewItem.self, forPrimaryKey: sender.tag)!.period
+        try! realm.commitWrite()
+        updateDelegate?.updateTableView()
     }
 }
