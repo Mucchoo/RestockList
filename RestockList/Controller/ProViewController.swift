ef713b03d1b09d91c0648c6e65738919c3dc83af
github user name <yazujumusa@gmail.com>
2022-05-09 10:39:02 +0900

    Add restoring purchase function

--- RestockList/Controller/ProViewController.swift
+++ RestockList/Controller/ProViewController.swift
@@ -12,12 +12,14 @@ class ProViewController: UIViewController {
 
     @IBOutlet var iconBackground: [UIView]!
     @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var restoreButton: UIButton!
     @IBOutlet var image: [UIImageView]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         iconBackground.forEach{ $0.layer.cornerRadius = 15}
         button.layer.cornerRadius = 20
+        restoreButton.layer.cornerRadius = 20
         image.forEach{ $0.layer.cornerRadius = 20 }
     }
     
@@ -32,10 +34,18 @@ class ProViewController: UIViewController {
         Purchases.shared.getOfferings { (offerings, error) in
             if let package = offerings?.current?.lifetime?.storeProduct {
                 Purchases.shared.purchase(product: package) { (transaction, customerInfo, error, userCancelled) in
-                    if customerInfo?.entitlements.all["Pro"]?.isActive == true {
-                        self.navigationController?.popToRootViewController(animated: true)
-                    }
                 }
+            }
+        }
+    }
+    
+    @IBAction func restoreButtonTapped(_ sender: UIButton) {
+        Purchases.shared.restorePurchases { customerInfo, error in
+            if customerInfo?.entitlements.all["Pro"]?.isActive == true {
+                let alert = UIAlertController(title: "購入を復元しました", message: "", preferredStyle: .alert)
+                let ok = UIAlertAction(title: "OK", style: .default)
+                alert.addAction(ok)
+                self.present(alert, animated: true)
             }
         }
     }
