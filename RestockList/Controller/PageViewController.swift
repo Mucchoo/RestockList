9e2d08775602c45c13219673f4733d8ede66a2fd
github user name <yazujumusa@gmail.com>
2022-05-27 09:59:59 +0900

    Fix PageView

--- RestockList/Controller/PageViewController.swift
+++ RestockList/Controller/PageViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class PageViewController: UIPageViewController {
     
     private var controllers: [UIViewController] = []
-    private let nextButton = UIButton()
+    private let usageLabel = UILabel()
     private let pageControl = UIPageControl()
     
     override func viewDidLoad() {
@@ -21,25 +21,26 @@ class PageViewController: UIPageViewController {
         let VC4 = storyboard!.instantiateViewController(withIdentifier: "Tutorial4")
         controllers = [VC1, VC2, VC3, VC4]
         setViewControllers([controllers[0]], direction: .forward, animated: true)
-        navigationController?.isNavigationBarHidden = true
         dataSource = self
-        view.addSubview(nextButton)
+        view.addSubview(usageLabel)
         view.addSubview(pageControl)
-        nextButton.setTitle("次へ", for: .normal)
-        nextButton.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .bold)
-        nextButton.backgroundColor = .white
-        nextButton.layer.cornerRadius = 20
-        Shadow.setTo(nextButton)
-        nextButton.translatesAutoresizingMaskIntoConstraints = false
-        nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        nextButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
-        nextButton.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width - 40).isActive = true
-        nextButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        usageLabel.layer.cornerRadius = 20
+        usageLabel.text = "使い方"
+        usageLabel.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        usageLabel.textAlignment = .center
+        usageLabel.backgroundColor = .white
+        usageLabel.layer.cornerRadius = 20
+        usageLabel.clipsToBounds = true
+        usageLabel.translatesAutoresizingMaskIntoConstraints = false
+        usageLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        usageLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
+        usageLabel.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width - 80).isActive = true
+        usageLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
         pageControl.numberOfPages = 4
         pageControl.currentPage = 0
         pageControl.translatesAutoresizingMaskIntoConstraints = false
         pageControl.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        pageControl.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -80).isActive = true
+        pageControl.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -70).isActive = true
         pageControl.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width - 40).isActive = true
         pageControl.heightAnchor.constraint(equalToConstant: 20).isActive = true
         pageControl.isUserInteractionEnabled = false
@@ -50,7 +51,7 @@ class PageViewController: UIPageViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         let theme = r.user.object(forKey: "theme") ?? 1
-        nextButton.setTitleColor(UIColor(named: "AccentColor\(theme)"), for: .normal)
+        usageLabel.textColor = UIColor(named: "AccentColor\(theme)")
     }
 }
 
@@ -67,7 +68,6 @@ extension PageViewController: UIPageViewControllerDataSource {
             return nil
         }
         index += 1
-        print("めくった")
         return controllers[index]
     }
     
@@ -75,15 +75,9 @@ extension PageViewController: UIPageViewControllerDataSource {
         var index = viewController.view.tag
         pageControl.currentPage = index
         index -= 1
-        print("もどした")
         if index < 0 {
             return nil
         }
         return controllers[index]
-    }
-    
-    func tutorialPageViewController(tutorialPageViewController: PageViewController,
-        didUpdatePageIndex index: Int) {
-        pageControl.currentPage = index
     }
 }
--- RestockList/Controller/Tutorial4ViewController.swift
+++ RestockList/Controller/Tutorial4ViewController.swift
@@ -13,17 +13,27 @@ class Tutorial4ViewController: UIViewController {
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var background: UIView!
     @IBOutlet weak var circle: UIView!
+    @IBOutlet weak var startButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         card.layer.cornerRadius = 20
-        Shadow.setTo(card)
+        startButton.layer.cornerRadius = 20
         circle.layer.cornerRadius = 400
+        Shadow.setTo(card)
+        Shadow.setTo(startButton)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         let theme = r.user.object(forKey: "theme") ?? 1
         image.tintColor = UIColor(named: "AccentColor\(theme)")
         background.backgroundColor = UIColor(named: "AccentColor\(theme)")
+        startButton.backgroundColor = UIColor(named: "AccentColor\(theme)")
     }
+    
+    @IBAction func startAction(_ sender: UIButton) {
+        self.dismiss(animated: true)
+    }
+    
 }
